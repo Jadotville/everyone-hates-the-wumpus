7dffb7d55749ea10d92cbaef70a69d7fccc034a9
Oliver B. <oliver.boettner.hst@gmail.com>
2024-11-19 13:38:16 +0100

    the game class consists of the grid preparation function and the simulation function
    the grid preparation function creates the grid, places the agents and gives them information
    
    the simulate function runs the game loop

--- environment.py
+++ environment.py
@@ -1,26 +1,48 @@
-class Game():
+class Game():   
     
-    # def __init__(self, size, num_pits, num_wumpi):
-    #     self.size = size
-    #     self.num_pits = num_pits
-    #     self.num_wumpi = num_wumpi
-    #     self.grid = self.create_grid()
-    #     self.place_pits()
-    #     self.place_wumpi()
-    #     self.place_player()
-    #     self.game_over = False
-    #     self.player_won = False
-    #     self.player = self.grid[self.player_pos[0]][self.player_pos[1]]
-    #     self.player.arrows = 1
-    #     self.player.score = 0
-    #     self.player.visited = set()
-    
-    def __init__(self, agents):
-        pass
-    
-    def create_grid(self):
+    def __init__(self):
         pass
     
     
-    def simulate(self):
-        pass
+    # creates a grid given the grid properties and places the agents in the corners
+    def grid_preperation(self, agents, grid_properties):
+        
+        # creates a grid of size n x n
+        size = grid_properties["size"]
+        grid = [[0 for _ in range(size)] for _ in range(size)]
+
+        
+        # places agents in the grid corners and gives them their positions and the grid size
+        if agents:
+            grid[0][0] = agents[0]  # Top-left corner
+            agents[0].position = (0, 0)
+            agents[0].grid_size = size
+        if len(agents) > 1:
+            grid[0][size - 1] = agents[1]  # Top-right corner
+            agents[1].position = (0, size - 1)
+            agents[1].grid_size = size
+        if len(agents) > 2:
+            grid[size - 1][0] = agents[2]  # Bottom-left corner
+            agents[2].position = (size - 1, 0)
+            agents[2].grid_size = size
+        if len(agents) > 3:
+            grid[size - 1][size - 1] = agents[3]  # Bottom-right corner
+            agents[3].position = (size - 1, size - 1)
+            agents[3].grid_size = size
+        
+        
+        # TODO: place pits
+        # TODO: place wumpi
+        # TODO: place gold
+        # TODO: place special items
+        
+        return grid
+    
+
+    # simulates the game given the agents and grid properties
+    def simulate(self, agents, grid_properties):
+        grid = self.grid_preperation(agents, grid_properties)
+        
+        # TODO: run the game loop
+        
+        
